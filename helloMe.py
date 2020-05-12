4d861d0e89fef984c61656b3e58b0389ab116bf1
Marco Madera <marcoseguro2@gmail.com>
2020-05-11 19:32:22 -0700

    if-else

--- helloMe.py
+++ helloMe.py
@@ -1,2 +1,10 @@
-name = str(input('¿Cuál es tu nombre?'))
-print('Hola, ' + name + '!')
+def main(): 
+  name = str(input('¿Cuál es tu nombre? '))
+  age = int(input('¿Cuál es tu edad? '))
+  if age > 18:
+    print('Hola, señor ' + name + '!')
+  else:
+    print('Hola, joven ' + name + '!')
+
+if __name__ == '__main__':
+  main()
